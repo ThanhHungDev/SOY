6245644ad47002ddf60034d57dd305973e6c17c1
ThanhHungDev <thanhhung.dev@gmail.com>
2019-09-14 11:36:49 +0700

    update code authenticate

--- index.js
+++ index.js
@@ -51,8 +51,10 @@ io.on('connection', function (socket) {
     // /////////////////////////////////////////////////////////////////////
     /**
      * vì cần socket chạy tiến trình song song
+     * I. trả về cho client 1 emit authentication_response mang thông tin các user trong 1 phòng
      * 1. xác thực người dùng là 1 promise 
      * 2. tìm kiếm channel tương ứng cũng là 1 promise
+     * II. set timeout cho token để nó có thể chat không gián đoạn
      */
     socket.on('authentication', async (data) => {
         /// variable define default
@@ -73,30 +75,51 @@ io.on('connection', function (socket) {
         .catch( error => false);
         /// cho 2 tuyến trình chạy cùng lúc thì ta cần đợi
         let auth = await check_auth;
-        let chanel = await get_channel;
+        let channel = await get_channel;
         /// lưu ý: khi auth đúng, mặc định ta luôn có channel đc trả ra nên sẽ join socket id vào channel
         if( auth ){
-            socket.join( chanel );
+            socket.join( channel );
             /// set data user cho socket
             socket.user_infor = data.authentication.user_infor;
-            socket.channel = chanel;
+            socket.channel = channel;
             /// bây giờ mình phải lấy tất cả user trong cùng 1 room/channel?
             var list_user_infor = [];
             /// thông qua mỗi room/channel, mình đều lấy đc list socket id đang onl
-            var list_socketid_in_room = io.sockets.adapter.rooms[chanel].sockets;
+            var list_socketid_in_room = io.sockets.adapter.rooms[channel].sockets;
             /// mình dùng thủ thuật để giải quyết bài toán trên bằng hàm : io.sockets.connected[socketid]
             /// loop id in list and connect to socket of id then get infor
             for (var socketid in list_socketid_in_room ) { 
                 var client_socket = io.sockets.connected[socketid];
                 list_user_infor.push(client_socket.user_infor);
             }
+            response.status = 200;
             response.data = [{
                 online : list_user_infor,
-                channel : chanel,
+                channel : channel,
                 socket : socket.id
             }];
         }
-        io.in(chanel).emit( 'authentication_response' , response );
+        io.in(channel).emit( 'authentication_response' , response );
+        /// lưu token đó gắn với channel trong 30p để khi 1 user chat 
+        /// sẽ gửi thông tin token lên server thì check điều kiện cần là 
+        /// token có đang thực sự dùng channel đó không?
+        /// điều kiện đủ là token còn thời gian sống không
+        REDIS.set( key_redis , access , 'EX', (CONFIG.TimeExpireAccessToken * 60), (err , status ) => {
+            if(err){
+                response = { status : 204 , message : "server refesh token fail" , data : [] };
+                io.in(channel).emit( 'server_fail' , response );
+            }
+            response = { status : 200 , message : "server refesh token ready" , data : [] };
+            io.in(channel).emit( 'ready_refesh' , response );
+        });
+        REDIS.set( access , channel , 'EX', (CONFIG.TimeExpireAccessToken * 60) , (err , status ) => {
+            if(err){
+                response = { status : 204 , message : "server refesh token fail" , data : [] };
+                io.in(channel).emit( 'server_fail' , response );
+            }
+            response = { status : 200 , message : "server refesh channel ready" , data : [] };
+            io.in(channel).emit( 'ready_set_channel' , response );
+        });
     })
     //////////////////////////////////////////////////////////
     socket.on('disconnect', function () {
@@ -191,7 +214,7 @@ app.post('/api/login', async (req, res)=>{
     if( !error ){
         var access = bcrypt.hashSync( refesh , salt );
         var key_redis = METHOD.renderKeyRedis( _user.id , client );
-        REDIS.set( key_redis , access , 'EX', (20 * 60) , (err , status ) => {
+        REDIS.set( key_redis , access , 'EX', (CONFIG.TimeExpireAccessToken * 60) , (err , status ) => {
             if(err){
                 error = { 
                     user_message : "authenticate fail", 
